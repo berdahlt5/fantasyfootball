2adf19ed70c30f95ade2e772efaf8e6297a20338
berdahlt5 <berdahlt5@gmail.com>
2026-09-13 18:47:40 -0400

    Load Crunch Time scenario mode assets

--- app-shell.js
+++ app-shell.js
@@ -247,10 +247,9 @@
   if (page === "crunchtime.html") {
     loadStyle("crunchTimeCommandCenterStyles","./crunchtime-command-center.css?v=20260913a");
     loadStyle("crunchTimePointsPlannerStyles","./crunchtime-points-planner.css?v=20260913c");
-    loadStyle("crunchTimeLiveContextStyles","./crunchtime-live-context.css?v=20260914b");
+    loadStyle("crunchTimeLiveContextStyles","./crunchtime-live-context.css?v=20260914d");
     loadScript("crunchTimeCommandCenterScript","./crunchtime-command-center.js?v=20260913a");
-    loadScript("crunchTimePointsPlannerScript","./crunchtime-points-planner.js?v=20260914b");
-    loadScript("crunchTimeTargetClarityScript","./crunchtime-target-clarity.js?v=20260913b");
+    loadScript("crunchTimePointsPlannerScript","./crunchtime-points-planner.js?v=20260914d");
     loadScript("crunchTimeLiveContextScript","./crunchtime-live-context.js?v=20260914c");
   }
 })();
