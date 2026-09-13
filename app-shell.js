858c6fa7cdfadcf63e3a9d383e3ccebd76e18661
berdahlt5 <berdahlt5@gmail.com>
2026-09-13 13:57:58 -0400

    Show live actual points in Player Pulse

--- app-shell.js
+++ app-shell.js
@@ -231,11 +231,13 @@
   if (page === "schedule-tool.html") {
     loadStyle("gameDayPulsePolishStyles","./gameday-pulse-polish.css?v=20260913b");
     loadStyle("gameDayPulseWindowStyles","./gameday-pulse-windows.css?v=20260913a");
+    loadStyle("gameDayPulseActualStyles","./gameday-pulse-actual.css?v=20260913a");
     loadStyle("gameDayScoreboardGlassStyles","./gameday-scoreboard-glass.css?v=20260913a");
     loadStyle("gameDayMatchupFootprintStyles","./gameday-matchup-footprint.css?v=20260913a");
 
     loadScript("gameDayPlayerPulseScript","./gameday-player-pulse-windowed.js?v=20260913a");
     loadScript("gameDayPulsePolishScript","./gameday-pulse-polish.js?v=20260913b");
+    loadScript("gameDayPulseActualScript","./gameday-pulse-actual.js?v=20260913a");
     loadScript("gameDayEnhancementsScript","./game-day-enhancements.js?v=20260913c");
     loadScript("gameDayHubDetailsScript","./gameday-hub-details.js?v=20260913a");
     loadScript("gameDayWatchRelevanceScript","./gameday-watch-relevance-v2.js?v=20260913a");
