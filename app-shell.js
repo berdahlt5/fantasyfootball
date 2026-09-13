ac4beb5da36d2972d09aa800d9d5fd1fcdc5404b
berdahlt5 <berdahlt5@gmail.com>
2026-09-13 17:58:14 -0400

    Bust cache for simplified Crunch Time planner

--- app-shell.js
+++ app-shell.js
@@ -246,8 +246,8 @@
 
   if (page === "crunchtime.html") {
     loadStyle("crunchTimeCommandCenterStyles","./crunchtime-command-center.css?v=20260913a");
-    loadStyle("crunchTimePointsPlannerStyles","./crunchtime-points-planner.css?v=20260913a");
+    loadStyle("crunchTimePointsPlannerStyles","./crunchtime-points-planner.css?v=20260913b");
     loadScript("crunchTimeCommandCenterScript","./crunchtime-command-center.js?v=20260913a");
-    loadScript("crunchTimePointsPlannerScript","./crunchtime-points-planner.js?v=20260913a");
+    loadScript("crunchTimePointsPlannerScript","./crunchtime-points-planner.js?v=20260913b");
   }
 })();
