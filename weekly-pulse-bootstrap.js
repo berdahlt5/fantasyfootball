db9714fa3e27ba407c8c3e5e79ab6f42a8354394
berdahlt5 <berdahlt5@gmail.com>
2026-09-13 18:21:59 -0400

    Refresh Player Pulse ranking assets in fallback loader

--- weekly-pulse-bootstrap.js
+++ weekly-pulse-bootstrap.js
@@ -29,10 +29,10 @@
     style("gameDayScoreboardGlassStyles","./gameday-scoreboard-glass.css?v=20260913a");
     style("gameDayMatchupFootprintStyles","./gameday-matchup-footprint.css?v=20260913a");
 
-    script("gameDayPlayerPulseScript","./gameday-player-pulse-windowed.js?v=20260913a");
+    script("gameDayPlayerPulseScript","./gameday-player-pulse-windowed.js?v=20260914a");
     script("gameDayPulsePolishScript","./gameday-pulse-polish.js?v=20260913b");
     script("gameDayPulseActualScript","./gameday-pulse-actual.js?v=20260913b");
-    script("gameDayPulseFinalRankingScript","./gameday-pulse-final-ranking.js?v=20260913a");
+    script("gameDayPulseFinalRankingScript","./gameday-pulse-final-ranking.js?v=20260914a");
     script("gameDayHubDetailsScript","./gameday-hub-details.js?v=20260913a");
     script("gameDayWatchRelevanceScript","./gameday-watch-relevance-v2.js?v=20260913a");
   }
